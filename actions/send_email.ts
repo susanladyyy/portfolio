4bc7d605c9575a40ea1a146842b747bf092b9337
susanladyyy <susanrumokoy02@gmail.com>
2024-06-26 20:11:26 +0700

    set api to production for the email

--- actions/send_email.ts
+++ actions/send_email.ts
@@ -5,6 +5,7 @@ import React from "react";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY)
+resend.apiKeys.create({ name: 'Production' });
 
 export const sendEmail = async (formData: FormData) => {
     const message = formData.get("message")
